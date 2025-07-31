7afa2259c414b9d23070f729d38b622beb27eee7
Dilasha <dilashavaidya@gmail.com>
2025-07-31 12:05:45 +0545

    Added modifications and jqueries

--- startbootstrap-modern-business-gh-pages/js/scripts.js
+++ startbootstrap-modern-business-gh-pages/js/scripts.js
@@ -5,3 +5,41 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+$(window).scroll(function() {
+  var scrollPosition = $(window).scrollTop();
+  
+  $('section').each(function() {
+    var currentSection = $(this);
+    var sectionTop = currentSection.offset().top - 100;
+    var sectionId = currentSection.attr('id');
+    
+    if (scrollPosition >= sectionTop) {
+      $('.nav-link').removeClass('active');
+      $('a[href="#' + sectionId + '"]').addClass('active');
+    }
+  });
+});
+
+// Back to Top Button
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 300) {
+    $('#backToTop').removeClass('d-none').fadeIn();
+  } else {
+    $('#backToTop').fadeOut();
+  }
+});
+
+$('#backToTop').click(function() {
+  $('html, body').animate({ scrollTop: 0 }, 800);
+});
+
+// Image zoom on hover
+$(".card-img-top").hover(
+  function() {
+    $(this).css("transform", "scale(1.05)");
+  },
+  function() {
+    $(this).css("transform", "scale(1)");
+  }
+);
