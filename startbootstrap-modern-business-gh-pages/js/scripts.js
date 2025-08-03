fb35648e127080f00580857f5b6994c2e5ac9891
Dilasha <dilashavaidya@gmail.com>
2025-08-03 10:59:01 +0545

    Colour pallete added into the home page

--- startbootstrap-modern-business-gh-pages/js/scripts.js
+++ startbootstrap-modern-business-gh-pages/js/scripts.js
@@ -6,20 +6,7 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-$(window).scroll(function() {
-  var scrollPosition = $(window).scrollTop();
-  
-  $('section').each(function() {
-    var currentSection = $(this);
-    var sectionTop = currentSection.offset().top - 100;
-    var sectionId = currentSection.attr('id');
-    
-    if (scrollPosition >= sectionTop) {
-      $('.nav-link').removeClass('active');
-      $('a[href="#' + sectionId + '"]').addClass('active');
-    }
-  });
-});
+
 
 
 // Image zoom on hover
